72299c2cab77c7b2e71081487f56ff4c80e6da1b
Julio <jc@JulioMacBook-Pro.local>
2026-06-04 21:45:33 +0200

    ui: perf

--- src/pages/index/Comp/Footer/footer.tsx
+++ src/pages/index/Comp/Footer/footer.tsx
@@ -4,18 +4,23 @@ export const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.information}>
-        <div>
-          Phone: 123-456-7890
-          <br />
-          Address: 123 Main St, Anytown, USA
-          <br />
-          Email: sajdjji@gamil.com
-          <br />
+        <div className={styles.contact}>
+          <div className={styles.contactRow}>
+            <span className={styles.label}>PHONE:</span>
+            <span className={styles.value}>+34 123-456-7890</span>
+          </div>
+          <div className={styles.contactRow}>
+            <span className={styles.label}>ADDRESS:</span>
+            <span className={styles.value}>123 Main St, Anytown, USA</span>
+          </div>
+          <div className={styles.contactRow}>
+            <span className={styles.label}>EMAIL:</span>
+            <span className={styles.value}>sajdjji@gamil.com</span>
+          </div>
         </div>
 
         <div className={styles.divider} />
-        <div>Version: 1.0.0</div>
-        <div>Copyright © 2024 Guia. All rights reserved.</div>
+        <div>COPYRIGHT © 2026 SALAMANCA.GO. All rights reserved.</div>
       </div>
     </div>
   );
