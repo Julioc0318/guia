0b5937467af4c4464544bf9726b2893f0c563940
Julio <jc@JulioMacBook-Pro.local>
2026-06-04 22:37:36 +0200

    ci: add github actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path"; // 1. 引入 path 模块
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 🚀 关键：改成你的 GitHub 仓库名称（前后都要有斜杠）
+  base: '/guia/', 
   resolve: {
     alias: {
-      // 2. 定义别名：把 @ 映射到项目根目录下的 src 文件夹
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
